965d7d37f2e61a7020c99691e5cc1268d6138d60
Master2Math <imammuktamarulhaq@gmail.com>
2024-08-02 21:45:11 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/pengguna',\App\Http\Controllers\UserController::class);
+//Route::resource('/pengguna',\App\Http\Controllers\UserController::class);
 // sudah
-Route::resource('/kelas',\App\Http\Controllers\KelasController::class);
+//Route::resource('/kelas',\App\Http\Controllers\KelasController::class);
 // sudah
 Route::resource('/customer',\App\Http\Controllers\CustomerController::class);
 Route::resource('/kamar',\App\Http\Controllers\KamarController::class);
